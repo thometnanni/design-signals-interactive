d50ad05b8a2118a9fedce2ae8e5559dfc3e54240
Fidel Thomet <git@ft0.ch>
2025-08-26 11:02:18 +0200

    fade in/out test

--- src/lib/Markdown.svelte
+++ src/lib/Markdown.svelte
@@ -1,26 +1,52 @@
 <script>
   import { marked } from "marked";
+  import { fade } from "svelte/transition";
 
   let { md } = $props();
 
-  let markdown = $derived(md && marked.parse(md));
+  let newHtml = $derived(md && marked.parse(md));
+
+  let currentHtml = $state(marked.parse(md)); // what is currently shown
+  let pendingHtml = null; // next html waiting
+  let show = $state(true); // controls visibility for fade
+
+  // Detect content change -> start fade out first
+
+  $effect(() => {
+    if (newHtml && newHtml !== currentHtml && newHtml !== pendingHtml) {
+      pendingHtml = newHtml;
+      if (show) show = false;
+    }
+  });
+
+  function handleOutroEnd() {
+    if (pendingHtml) {
+      currentHtml = pendingHtml;
+      pendingHtml = null;
+    }
+    show = true; // triggers in:fade after old fully gone
+  }
 </script>
 
 <section class="info">
-  {@html markdown}
+  {#if show}
+    <div
+      class="markdown-fragment"
+      in:fade={{ duration: 750 }}
+      out:fade={{ duration: 750 }}
+      onoutroend={handleOutroEnd}
+    >
+      {@html currentHtml}
+    </div>
+  {/if}
 </section>
 
 <style>
   .info {
-    /* background-color: var(--secondary); */
     padding: 1vh 5vh;
-
     border-left: 1px solid var(--secondary);
-
     color: var(--primary);
-
     font-size: 24px;
-
     :global(h1) {
       font-size: 3em;
       color: var(--secondary);
